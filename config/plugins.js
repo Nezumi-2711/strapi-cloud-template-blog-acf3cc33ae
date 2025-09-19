8099469aacffed10a1989f9d968e95e48b7b8d5c
Nezumi-2711 <phanhuuloi15@gmail.com>
2025-09-19 22:20:07 +0700

    integrate with supabase

--- config/plugins.js
+++ config/plugins.js
@@ -6,9 +6,27 @@ module.exports = ({ env }) => ({
 				apiKey: env('RESEND_API_KEY'), // Required
 			},
 			settings: {
-				defaultFrom: 'me@example.com',
-				defaultReplyTo: 'me@example.com',
+				defaultFrom: env('RESEND_DEFAULT_FROM'),
+				defaultReplyTo: env('RESEND_DEFAULT_REPLY_TO'),
 			},
 		},
 	},
+
+	upload: {
+    config: {
+      provider: "strapi-provider-upload-supabase",
+      providerOptions: {
+        apiUrl: env("SUPABASE_API_URL"),
+        apiKey: env("SUPABASE_API_KEY"),
+        bucket: env("SUPABASE_BUCKET"),
+        directory: env("SUPABASE_DIRECTORY"),
+        options: {},
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
